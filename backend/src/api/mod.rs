f1fc722dcded517800936ec8f59ffe1c2f787b7c
Nikita Strygin <nikita6@bk.ru>
2022-09-16 19:27:54 +0300

    Improve error handling, add /ping endpoint for liveliness/readiness checks

--- backend/src/api/mod.rs
+++ backend/src/api/mod.rs
@@ -3,11 +3,16 @@ mod models;
 use crate::db::{DbData, GetSessions};
 use crate::error::ResponseResult;
 use actix_web::{get, post, web, web::ServiceConfig, HttpResponse, Responder};
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world! (this is a stub 6)")
+}
+
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::Ok().body("pong")
 }
 
 #[post("/echo")]
@@ -22,8 +27,17 @@ async fn get_sessions(db: DbData) -> ResponseResult<web::Json<Vec<models::Sessio
     Ok(web::Json(sessions.into_iter().map(|v| v.into()).collect()))
 }
 
+#[get("/error")]
+async fn error() -> ResponseResult<web::Bytes> {
+    Err(anyhow!("Example error").into())
+}
+
 pub fn configure() -> Result<impl Fn(&mut ServiceConfig) + Clone> {
     Ok(|cfg: &mut ServiceConfig| {
-        cfg.service(hello).service(echo).service(get_sessions);
+        cfg.service(hello)
+            .service(ping)
+            .service(echo)
+            .service(get_sessions)
+            .service(error);
     })
 }
